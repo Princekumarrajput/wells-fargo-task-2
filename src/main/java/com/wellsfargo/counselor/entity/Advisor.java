b23f0e0a2a82090e66fc26f27567957187767f0f
Prince kumar <princekumarrajput786b@gmail.com>
2026-01-21 22:48:38 +0530

    Update Advisor.java

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -7,80 +7,15 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 
 @Entity
-public class Advisor {
-
-    @Id
-    @GeneratedValue()
-    private long advisorId;
-
-    @Column(nullable = false)
-    private String firstName;
-
-    @Column(nullable = false)
-    private String lastName;
-
-    @Column(nullable = false)
-    private String address;
-
-    @Column(nullable = false)
-    private String phone;
-
-    @Column(nullable = false)
-    private String email;
-
-    protected Advisor() {
-
-    }
-
-    public Advisor(String firstName, String lastName, String address, String phone, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
-    }
-
-    public Long getAdvisorId() {
-        return advisorId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+public interface FinancialAdvisorRepository extends JpaRepository<FinancialAdvisor, Long> {
+    List<FinancialAdvisor> findByNameContainingIgnoreCase(String name);
+}
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    List<Client> findByFinancialAdvisor_AdvisorId(Long advisorId);
+}
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+    Portfolio findByClient_ClientId(Long clientId);
+}
+public interface SecurityRepository extends JpaRepository<Security, Long> {
+    List<Security> findByPortfolio_PortfolioId(Long portfolioId);
 }
